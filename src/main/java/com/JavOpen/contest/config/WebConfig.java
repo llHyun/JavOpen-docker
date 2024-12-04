ad69459f226c259fcb1ebde3ed9c9ebae6b14a4c
llHyun <dorazy123@gmail.com>
2024-12-04 15:27:27 +0900

    feat: project build

--- src/main/java/com/JavOpen/contest/config/WebConfig.java
+++ src/main/java/com/JavOpen/contest/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든 엔드포인트 허용
                 .allowedOrigins("http://testmynewbuket456.s3-website.ap-northeast-2.amazonaws.com/",
                         "https://dl0y1h8935ths.cloudfront.net",
-                        "http://dl0y1h8935ths.cloudfront.net") // React 개발 서버 URL
+                        "http://dl0y1h8935ths.cloudfront.net",
+                        "http://10.101.134.118:3000") // React 개발 서버 URL
                 .allowedMethods("*") // 모든 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 및 인증 정보 허용
